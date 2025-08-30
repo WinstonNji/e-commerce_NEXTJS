60adc9f941d08d5e506f0a1dc6f05cc45a89dca7
Winston Nji <winston01russel@gmail.com>
2025-08-30 21:30:26 +0200

    feat: Add public assets and images for e-commerce site
    - Add product images (headphones, skincare, gardening tools, etc.)
    - Include SVG icons for UI elements (credit card, delivery, headphones)
    - Add JavaScript files for assets and product data
    - Set up public directory structure for Next.js static assets

---
+++ app/public/assets.js
@@ -0,0 +1,12 @@
+import freeShippingIcon from './free-delivery_411776.svg'
+import thirtyDayReturnIcon from './30-days_11622528.svg'
+import customerServiceIcon from './headphones_2964082.svg'
+import creditCardSecureIcon from './credit-card_677138.svg'
+
+export const assets = {
+    freeShippingIcon,
+    thirtyDayReturnIcon,
+    customerServiceIcon,
+    creditCardSecureIcon
+    
+}
