86bf00494e31f4fd6828f0d105653a47a1cba057
Winston Nji <winston01russel@gmail.com>
2025-10-06 17:11:42 +0200

    chore(next): update next.config for project setup

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images : {
-        remotePatterns : [{
-            protocol: 'https',
-            hostname : 'cdn.dummyjson.com',
-            port : '',
-            pathname: '/**'
-        }]
+        remotePatterns : [
+            {
+                protocol: 'https',
+                hostname : 'cdn.dummyjson.com',
+                port : '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname : 'www.aputf.org',
+                port : '',
+                pathname: '/**'
+            }
+
+        ]
     }
 };
 
