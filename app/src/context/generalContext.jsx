f01e8d0ca018ef295c2fb1f842a7b94a65e24609
Winston Nji <winston01russel@gmail.com>
2025-11-03 18:33:31 +0200

    Create GeneralContext for shared category and brand fetching

---
+++ app/src/context/generalContext.jsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { createContext } from "react"
+import { toast } from "react-toastify"
+import React from 'react'
+
+export const GeneralContext = createContext()
+
+function GeneralContextProvider({children}) {
+
+const fetchCategory = async () => {
+    try {
+        const res = await fetch('/api/v1/admin/category', {cache: 'force-cache'})
+        if(!res.ok){
+            throw new Error('Failed to create new product')
+        }
+        const result = await res.json()
+        return result.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const fetchBrands = async () => {
+    try {
+        const res = await fetch('/api/v1/general/brand', {cache : 'force-cache'})
+        if(!res.ok){
+            throw new Error("An error occured, couldn't fetch brands")
+        }
+        const result = await res.json()
+        if(!result){
+            throw new Error("Couldn't get brands")
+        }
+        return result.data
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+  return (
+    <GeneralContext.Provider value={{fetchCategory,fetchBrands}}>
+        {children}
+    </GeneralContext.Provider>
+  )
+}
+
+export default GeneralContextProvider
